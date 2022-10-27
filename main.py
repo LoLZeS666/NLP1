89b066946714c654238972d8b595fed2448f1ebf
Ayush <ayush01.dhoot@gmail.com>
2022-10-27 16:23:47 +0530

    added tag freq

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import operator
-
 import nltk
 from matplotlib import pyplot as plt
 import string
@@ -13,14 +12,18 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 from wordcloud import WordCloud, STOPWORDS
+from pattern.text.en import singularize
+import inflect
 
 #read book
-t1 = open("test.txt", encoding="utf8")
+t1 = open("test2.txt", encoding="utf8")
 
 text = []
 words = []
 final = []
 lemmatizer = WordNetLemmatizer()
+custom = ["chapter", "page", ]
+p = inflect.engine()
 
 #helper function
 def get_wordnet_pos(treebank_tag):
@@ -43,10 +46,10 @@ for line in t1:
 stop_words = set(stopwords.words('english'))
 
 for sentence in text:
-    punctuationfree = "".join([i for i in sentence if i not in string.punctuation])  #remove punctuation
+    punctuationfree = "".join([i for i in sentence if i not in string.punctuation and not i.isdigit()])  #remove punctuation
     word_tokens = word_tokenize(punctuationfree)   #tokenize words
     filtered_sentence = nltk.pos_tag(word_tokens)  #part of speech tagging for words based on treebank tagset
-    filtered_sentence = [(w.casefold(), t) for w, t in filtered_sentence if not w.lower() in STOPWORDS] #remove stopwords
+    filtered_sentence = [(singularize(w.casefold()), t) for w, t in filtered_sentence if not w.lower() in STOPWORDS and len(w)>1 and w.lower() not in custom] #remove stopwords
     words.append(filtered_sentence)
 
 for sentence in words:
@@ -56,33 +59,41 @@ for sentence in words:
     final.append(temp)
 
 
-
 #word frequency plots
 to_plot = []
+to_plot_tag = []
 for sentence in words:
     for word, tag in sentence:
         to_plot.append(word)
+        to_plot_tag.append(tag)
 counted = Counter(to_plot)
+counted_tag = Counter(to_plot_tag)
 word_freq = pd.DataFrame(counted.items(),columns=['word','frequency']).sort_values(by='frequency',ascending=False)
-
+tag_freq = pd.DataFrame(counted_tag.items(), columns=['tag', 'frequency']).sort_values(by='frequency', ascending=False)
 mp = defaultdict(int)
 for word in word_freq['word']:
     mp[len(word)] += 1
 mp = dict( sorted(mp.items(), key=operator.itemgetter(1),reverse=True))
 keys = list(mp.keys())
 vals = [mp[k] for k in keys]
-print(keys)
-print(vals)
+
+keys = keys[:15]
+vals = vals[:15]
 sns.barplot(x=keys, y=vals)
 plt.title("Word length vs frequency")
 plt.xlabel("Word Length")
 plt.ylabel("Frequency")
 plt.show()
 
-word_freq = word_freq.head(30)
+word_freq = word_freq.head(25)
+plt.title("Word frequency")
 sns.barplot(x='frequency', y='word', data=word_freq)
 plt.show()
 
+tag_freq = tag_freq.head(10)
+plt.title("Tag Frequency (Treebank Tags)")
+sns.barplot(x='frequency', y='tag', data=tag_freq)
+plt.show()
 
 #wordcloud
 temp = []
